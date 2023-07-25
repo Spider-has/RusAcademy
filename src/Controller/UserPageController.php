2da9a7933fa1a15f826e124ab1a98e48b6eded14
Konstantin Kalinin <exevald@Air-Konstantin.bbrouter>
2023-07-25 23:47:23 +0300

    Удалил ненужные файлы из проекта. Организовал работу с twig шаблонами. Написал тестовый контроллер

---
+++ src/Controller/UserPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Views\Twig;
+
+class UserPageController
+{
+    public function userPage(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, "layout.html.twig");
+    }
+}
