c106ff59ea8f3663bdd144307b5ad2f1956058a6
Konstantin Kalinin <exevald@Air-Konstantin.bbrouter>
2023-07-28 01:56:22 +0300

    Хорошая попытка подключить реакт с твигом

---
+++ public/build/app.js
@@ -0,0 +1,37 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["app"],{
+
+/***/ "./assets/app.tsx":
+/*!************************!*\
+  !*** ./assets/app.tsx ***!
+  \************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+
+__webpack_require__(/*! core-js/modules/es.object.define-property.js */ "./node_modules/core-js/modules/es.object.define-property.js");
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+var jsx_runtime_1 = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+var client_1 = __importDefault(__webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js"));
+var root = client_1["default"].createRoot(document.getElementById('root'));
+root.render((0, jsx_runtime_1.jsx)("h1", {
+  children: "Hello, world!"
+}));
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors-node_modules_core-js_modules_es_object_define-property_js-node_modules_react-dom_clie-f2f794"], () => (__webpack_exec__("./assets/app.tsx")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUNBLElBQUFBLFFBQUEsR0FBQUMsZUFBQSxDQUFBQyxtQkFBQTtBQUVBLElBQU1DLElBQUksR0FBR0gsUUFBQSxXQUFRLENBQUNJLFVBQVUsQ0FBQ0MsUUFBUSxDQUFDQyxjQUFjLENBQUMsTUFBTSxDQUFDLENBQUM7QUFDakVILElBQUksQ0FBQ0ksTUFBTSxDQUFDLElBQUFDLGFBQUEsQ0FBQUMsR0FBQTtFQUFBQyxRQUFBO0FBQUEsRUFBc0IsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2Fzc2V0cy9hcHAudHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCc7XG5pbXBvcnQgUmVhY3RET00gZnJvbSAncmVhY3QtZG9tL2NsaWVudCc7XG5cbmNvbnN0IHJvb3QgPSBSZWFjdERPTS5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykpO1xucm9vdC5yZW5kZXIoPGgxPkhlbGxvLCB3b3JsZCE8L2gxPik7XG4iXSwibmFtZXMiOlsiY2xpZW50XzEiLCJfX2ltcG9ydERlZmF1bHQiLCJyZXF1aXJlIiwicm9vdCIsImNyZWF0ZVJvb3QiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIiwianN4X3J1bnRpbWVfMSIsImpzeCIsImNoaWxkcmVuIl0sInNvdXJjZVJvb3QiOiIifQ==
