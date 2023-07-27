5481c8554b45bc48f6ebacd462bc10402a3fb915
Spider <96444071+Spider-has@users.noreply.github.com>
2023-07-27 12:54:29 +0300

    пощупал твиг

--- src/Controller/UserPageController.php
+++ src/Controller/UserPageController.php
@@ -13,6 +13,6 @@ class UserPageController
     public function userPage(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $view = Twig::fromRequest($request);
-        return $view->render($response, "layout.html.twig");
+        return $view->render($response, "main.html.twig");
     }
 }
